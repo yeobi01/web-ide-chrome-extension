e676061de2e9436ae69e55a644e69d1800b15fd3
yeobi01 <yeobi_01@naver.com>
2025-03-11 17:28:57 +0900

    [INIT] tailwindcss 셋팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
       input: {
